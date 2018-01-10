72bb776c8d4e01ab7f3248fc7b75ccf6aa0166bf
Sahak <hakobyan.sahak88@gmail.com>
2018-01-10 16:43:11 +0400

    up

--- src/Http/Controllers/DataTablesConroller.php
+++ src/Http/Controllers/DataTablesConroller.php
@@ -12,7 +12,7 @@ class DataTablesConroller extends Controller
     public function getPlans()
     {
         return DataTables::of(Plans::query())->addColumn('actions', function ($plans) {
-            $url= url("admin/membership/plan/edit",$plans->id);
+            $url= url("admin/membership/plans/edit",$plans->id);
             return "<a href='$url' class='btn btn-warning'><i class='fa fa-edit'></i></a>";
         },2)->rawColumns(['actions'])->make(true);
   }
