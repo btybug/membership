5385e48f192b81ca7ae088b33d271c80e9a083e7
gev2092 <gev2092harutyunyan@gmail.com>
2018-01-10 04:25:56 -0800

    create

--- src/Http/Controllers/PlansController.php
+++ src/Http/Controllers/PlansController.php
@@ -5,6 +5,7 @@ namespace BtyBugHook\Membership\Http\Controllers;
 use App\Http\Controllers\Controller;
 use BtyBugHook\Membership\Repository\PlansRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PlansController extends Controller
 {
@@ -16,8 +17,26 @@ class PlansController extends Controller
         return view('forms::plans.edit');
     }
     public function saveCreatePlan(PlansRepository $repo, Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'price' => 'required|integer',
+            'period' => 'required|integer',
+            'period_type' => 'required',
+            'currency' => 'required',
+            'is_active' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
         $data = $request->all();
-        $repo->model()->create($data);
+        $result = $repo->model()->create($data);
+
+        return redirect()->route('mbsp_plans');
     }
     public function updatePlan(PlansRepository $repo, Request $request,$id){
         $data = $request->all();
--- src/views/plans/create.blade.php
+++ src/views/plans/create.blade.php
@@ -2,27 +2,28 @@
 @section('content')
     <div class="main_lay_cont">
 
-        <form action="" class="bty-form-4" action="{{route('mbsp_plans_create_save')}}">
+        <form action="" class="bty-form-4" method="post" action="{{route('mbsp_plans_create_save')}}">
+            {{csrf_field()}}
             <h2>Create Plan</h2>
             <fieldset class="bty-form-text">
                 <legend><span><i class="fa fa-info" aria-hidden="true"></i></span>General</legend>
                 <div>
-                    <input class="bty-input-label-6" name="title" type="text" placeholder="Insert Title">
+                    <input class="bty-input-label-6" name="title" type="text" placeholder="Insert Title" value="{{old('title')}}">
                     <label>Title?</label>
                 </div>
                 <div>
-                    <input class="bty-input-label-6" name="price" type="number" min="1" placeholder="Insert Price">
+                    <input class="bty-input-label-6" name="price" type="number" min="1" placeholder="Insert Price" value="{{old('price')}}">
                     <label>Price?</label>
                 </div>
                 <div>
-                    <input class="bty-input-label-6" name="period" type="number" placeholder="Insert Period">
+                    <input class="bty-input-label-6" name="period" type="number" placeholder="Insert Period" value="{{old('period')}}">
                     <label>Period?</label>
                 </div>
             </fieldset>
             <fieldset class="bty-form-select">
                 <legend><span><i class="fa fa-info" aria-hidden="true"></i></span>Period Type</legend>
                 <div class="bty-input-select-3">
-                    <select name="period_type">
+                    <select name="period_type" {{old('period_type')}}>
                         <option>day</option>
                         <option>week</option>
                         <option>month</option>
@@ -31,14 +32,14 @@
             </fieldset>
             <fieldset class="bty-form-text">
                 <div>
-                    <input class="bty-input-label-6" name="currency" type="text" placeholder="Insert Currency">
+                    <input class="bty-input-label-6" name="currency" type="text" placeholder="Insert Currency" value="{{old('currency')}}">
                     <label>Currency?</label>
                 </div>
                 <fieldset class="bty-form-radio">
                     <legend><span><i class="fa fa-info" aria-hidden="true"></i></span>Is Active</legend>
                     <div>
                         <input name="is_active" value="1" type="radio" class="bty-input-radio-1"
-                               id="bty-gender-form-61">
+                               id="bty-gender-form-61" checked>
                         <label for="bty-gender-form-61">Yes</label>
                         <input name="is_active" value="0" type="radio" class="bty-input-radio-1"
                                id="bty-gender-form-62">
