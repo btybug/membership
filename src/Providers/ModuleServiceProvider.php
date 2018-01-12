8282aa4347ddfeb51cc2604a64fcafd727786414
sahakavatar <hakobyan.sahak88@gmail.com>
2018-01-12 23:11:35 +0400

    up

--- src/Providers/ModuleServiceProvider.php
+++ src/Providers/ModuleServiceProvider.php
@@ -82,8 +82,8 @@ class ModuleServiceProvider extends ServiceProvider
                 ],
             ]
         ];
-        \Config::set('painter.PAINTERSPATHS',array_merge( \Config::get('painter.PAINTERSPATHS'),['app'.DS.'Plugins'.DS.'vendor'.DS.'sahak.avatar'.DS.'membership'.DS.'src'.DS.'Units']));
 
+        \Config::set('painter.PAINTERSPATHS',array_merge( \Config::get('painter.PAINTERSPATHS'),['app'.DS.'Plugins'.DS.'vendor'.DS.'sahak.avatar'.DS.'membership'.DS.'src'.DS.'Units']));
         \Eventy::action('my.tab', $tubs);
         Routes::registerPages('sahak.avatar/membership');
     }
