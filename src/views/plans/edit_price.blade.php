81e74803c765f9b401781f166da3a2905cfb4971
sahakavatar <hakobyan.sahak88@gmail.com>
2018-01-13 22:44:39 +0400

    up

--- src/views/plans/edit_price.blade.php
+++ src/views/plans/edit_price.blade.php
@@ -8,19 +8,17 @@
         <!-- Form Name -->
         <legend>Edit Plan Price {!! $plan->name !!}</legend>
 
-    </div>
-    </div>
-    <!-- Select Basic -->
-    <div class="form-group">
-    <label class="col-md-4 control-label" for="amount">Select Price Plans </label>
-    <div class="col-md-4">
-    {!! Form::select('amount',[null=>'No Price'],null,['class'=>'form-control','id'=>'amount']) !!}
-    </div>
-    </div>
+        <!-- Select Basic -->
+        <div class="form-group">
+            <label class="col-md-4 control-label" for="amount">Select Price Plans </label>
+            <div class="col-md-4">
+                {!! Form::select('amount',[null=>'No Price'],null,['class'=>'form-control','id'=>'amount']) !!}
+            </div>
+        </div>
         <!-- Button -->
         <div class="form-group">
             <div class="col-md-4">
-                <input type="submit" id="singlebutton"  class="btn btn-primary">
+                <input type="submit" id="singlebutton" class="btn btn-primary">
             </div>
         </div>
     </fieldset>
