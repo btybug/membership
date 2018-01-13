245f06e02d330e9b0920a69cce16c64bd8b0b101
sahakavatar <hakobyan.sahak88@gmail.com>
2018-01-13 22:43:16 +0400

    up

--- src/views/plans/edit_price.blade.php
+++ src/views/plans/edit_price.blade.php
@@ -1,6 +1,30 @@
 @extends('btybug::layouts.mTabs',['index'=>'create_product','id'=>$id])
 <!-- Nav tabs -->
 @section('tab')
+    {!! Form::model($plan,['class'=>'form-horizontal','url'=>route('mbsp_plans_edit_save',$plan->id)]) !!}
+
+    <fieldset>
+
+        <!-- Form Name -->
+        <legend>Edit Plan Price {!! $plan->name !!}</legend>
+
+    </div>
+    </div>
+    <!-- Select Basic -->
+    <div class="form-group">
+    <label class="col-md-4 control-label" for="amount">Select Price Plans </label>
+    <div class="col-md-4">
+    {!! Form::select('amount',[null=>'No Price'],null,['class'=>'form-control','id'=>'amount']) !!}
+    </div>
+    </div>
+        <!-- Button -->
+        <div class="form-group">
+            <div class="col-md-4">
+                <input type="submit" id="singlebutton"  class="btn btn-primary">
+            </div>
+        </div>
+    </fieldset>
+    {!! Form::close() !!}
 
 @stop
 @section('CSS')
