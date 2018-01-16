23f7fa7c87d0f3a6fc111a2762d012eea50552ed
Sahak <hakobyan.sahak88@gmail.com>
2018-01-16 16:27:33 +0400

    up

--- src/views/settings/index.blade.php
+++ src/views/settings/index.blade.php
@@ -3,28 +3,7 @@
 @section('tab')
     <div class="main_lay_cont">
     <div class="col-md-5">
-        {!! Form::open(['url'=>route('mbsp_settings_save_pricing_page')]) !!}
-        <div class="bty-panel-collapse">
-
-            <div>
-                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true">
-                    <span class="icon"><i class="fa fa-chevron-down" aria-hidden="true"></i></span>
-                    <span class="title">Pricing page settings</span>
-                </a>
-            </div>
-            <div id="collapseOne" class="collapse in" aria-expanded="true" style="">
-                <div class="content">
-                    {!! BBbutton2('unit','pricing','products','Select Pricing Unit') !!}
-                </div>
-                <div class="content">
-                    {!! BBbutton2('unit','product','product','Select Product Pnit') !!}
-                </div>
-            </div>
-            <div class="col-md-3">
-                <button type="submit" class="bty-btn bty-btn-save"><span>Save</span></button>
-            </div>
-        </div>
-        {!! Form::close() !!}
+       
     </div>
     </div>
     @include('resources::assests.magicModal')
