dea0aec61837da0951683a63be3d2405a45bf95a
Eduard <edo.terakyan@gmail.com>
2018-02-12 20:59:26 +0400

    form width

--- src/views/common/edit.blade.php
+++ src/views/common/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('btybug::layouts.admin')
 @section('content')
-    <div class="col-md-8">
+    <div class="col-md-12">
         [form slug=create_{{$slug}} data-id={{ $post->id }} edit=id]
     </div>
 @stop
